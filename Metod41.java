aca75a71b85e87e6a644a0e646742f4b31856d7f
Umida Turdiyeva <umidabunyod@bk.ru>
2022-03-13 14:16:21 +0500

    Metod41

---
+++ Metod41.java
@@ -0,0 +1,56 @@
+// import java.util.Scanner;
+
+ 
+//  public class Metod41 {
+
+
+//     public static void main(String[] args) {
+//         MinMax27();
+//         // misol40(1);
+//      }
+    
+// //     public static void misol40(double x) {
+// //         double surat = 1;
+// //         double maxraj = 1;
+// //         double e = 2.71;
+// //         double s = 0;
+// //         double i = 1;
+// //         while(s > e){
+// //             surat  = surat * x;
+// //             maxraj  = maxraj * i;
+// //             i++;
+// //             s = s + ((double) (surat/maxraj));
+// //         }
+// //         double natija = s + 1;
+// //         System.out.println(natija);
+        
+// //     }
+
+
+// public static void MinMax27() {
+//     Scanner sc = new Scanner(System.in);    
+//     int n = sc.nextInt();
+//     int max = 0;
+//     int soni1 = 0;
+//     int soni2 = 0;
+//     int index = 0;
+//     for(int i=0; i<n; i++){
+//         int a = sc.nextInt();        
+//             if(a==0){
+//                 soni1++;
+//                 soni2 = max;
+//             }else if(a==1){
+//                 soni2++;
+//                 soni1 = max;
+//             }
+//             if(max < soni2){
+//                 max = soni2;
+//             }else if ( max < soni1){
+//                 max = soni1;
+//             }
+//     }
+//     System.out.println(max);
+//     // System.out.println(index);
+//  }
+
+//  }
